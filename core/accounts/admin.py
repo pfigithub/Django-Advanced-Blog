34b8abfe5d71c2e54996fe92bab233e1c8169ff7
pfigithub <pampam7072@gmail.com>
2023-05-22 14:49:14 +0330

    static and media and user model
    making ready static and media roots and url dirs, auth user model and staticfiles dirs
    
    making ready main urls:
    serving static and media for dev

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import User, Profile
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    search_fields = ("email",)
+    ordering = ("email",)
+
+    fieldsets = (
+        ('Authentication', {"fields": ("email",)}),
+        ("Permissions", {"fields": ("is_superuser", "is_staff", "is_active", "groups", "user_permissions", "last_login")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_superuser", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+
+
+admin.site.register(Profile)
+admin.site.register(User, CustomUserAdmin)

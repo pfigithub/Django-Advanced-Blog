3b41bcd41287b83609895d2a36c3c023f13a150c
pfigithub <pampam7072@gmail.com>
2023-05-22 14:57:07 +0330

    app blog:Post
    Post and category, PostAdmin, migrations, remove tag temporary, and create a post by create media file

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class Post(models.Model):
+    '''this is a class to define posts for blog app'''
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    created_date = models.TimeField(auto_now_add=True)
+    updated_date = models.TimeField(auto_now=True)
+    published_date = models.TimeField()
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    '''class for category'''
+
+    name = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name

d60c63001940c57469cb708446a44e338a963c23
pfigithub <pampam7072@gmail.com>
2023-06-22 11:43:04 +0330

    Update models.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 from django.utils.translation import gettext_lazy as _
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -12,6 +16,7 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
+
     def create_user(self, email, password, **extra_fields):
         """
         Create and save a user with the given email and password.
@@ -31,24 +36,23 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_verified", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
         return self.create_user(email, password, **extra_fields)
-    
-
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    '''custom user model form for our app'''
+    """custom user model form for our app"""
 
     email = models.EmailField(max_length=255, unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    #is_verified = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
@@ -59,7 +63,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-    
+
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -72,8 +76,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
-    
-@receiver(post_save, sender= User)
+
+
+@receiver(post_save, sender=User)
 def save_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)

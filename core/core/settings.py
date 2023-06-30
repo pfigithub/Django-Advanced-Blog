5d4315958b6b966036626532bfb476d524596343
pfigithub <pampam7072@gmail.com>
2023-06-30 16:29:36 +0330

    celery
    celery and redis

--- core/core/settings.py
+++ core/core/settings.py
@@ -50,11 +50,13 @@ INSTALLED_APPS = [
     "drf_yasg",
     "rest_framework_simplejwt",
     "mail_templated",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -168,3 +170,8 @@ EMAIL_HOST = "smtp4dev"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 25
+
+CORS_ALLOW_ALL_ORIGINS: True
+
+# celery configs
+CELERY_BROKER_URL = "redis://redis:6379/1"
